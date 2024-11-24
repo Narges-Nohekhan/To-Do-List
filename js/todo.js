9e010caa55a926232b199802b086766c2f215b38
Narge <nargesnovhekhan@gmail.com>
2024-11-24 21:25:22 +0330

    Adding Css styles to the project.

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,14 @@
 let addBtn = document.querySelector('button');
 let taskList = document.querySelector('ul');
 let input = document.querySelector('input');
-addBtn.addEventListener('click' , ()=>{
+addBtn.addEventListener('click', () => {
     let text = input.value;
     let task = createTask(text);
     taskList.appendChild(task);
     input.value = '';
 })
 
-function createTask(text){
+function createTask(text) {
     let li = document.createElement('li');
     li.textContent = text;
     return li;
