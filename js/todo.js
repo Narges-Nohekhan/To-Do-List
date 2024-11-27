d07c2bb08261d3161a4bf8f2fd0a06742d139034
Narges <nargesnovhekhan@gmail.com>
2024-11-27 16:34:08 +0330

    Add the ability to tick tasks to the project and end the project.

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,26 @@
 let addBtn = document.querySelector('button');
 let taskList = document.querySelector('ul');
 let input = document.querySelector('input');
+let errorMessage = document.querySelector('.error_message');
 addBtn.addEventListener('click', () => {
     let text = input.value;
+    if (text === '' || text.split(' ').join('') === '') {
+        // input.value = null;
+        errorMessage.textContent = 'ورودی باید شامل متن باشد'
+        return;
+    }
+    errorMessage.textContent = '';
     let task = createTask(text);
     task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>';
     taskList.appendChild(task);
     input.value = '';
 })
-taskList.addEventListener('click' , (e)=>{
-    if (e.target.nodeName === 'I'){
+taskList.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'I') {
         e.target.parentElement.parentElement.style = 'display : none';
+    }
+    if (e.target.nodeName === 'LI') {
+        e.target.classList.toggle('done');
     }
 })
 
