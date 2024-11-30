bad611c2474b5f51f23c50ccec42d1f60ceb4fc1
Narges <nargesnovhekhan@gmail.com>
2024-11-30 17:42:04 +0330

    Saving tasks in local storage using JavaScript

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ let addBtn = document.querySelector('button');
 let taskList = document.querySelector('ul');
 let input = document.querySelector('input');
 let errorMessage = document.querySelector('.error_message');
+let tasks;
+if (!localStorage.getItem('todo')){
+    tasks = [];
+}else {
+    tasks = getTasks();
+}
 addBtn.addEventListener('click', () => {
     let text = input.value;
     if (text === '' || text.split(' ').join('') === '') {
@@ -13,6 +19,7 @@ addBtn.addEventListener('click', () => {
     let task = createTask(text);
     task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>';
     taskList.appendChild(task);
+    saveTasks(text);
     input.value = '';
 })
 taskList.addEventListener('click', (e) => {
@@ -28,4 +35,12 @@ function createTask(text) {
     let li = document.createElement('li');
     li.textContent = text;
     return li;
+}
+
+function saveTasks(text) {
+    tasks.push(text);
+    localStorage.setItem('todo', tasks);
+}
+function getTasks(){
+    return localStorage.getItem('todo').split(',');
 }
